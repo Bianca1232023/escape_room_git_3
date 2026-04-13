a181cb7c2098092cd71b245db9190bd96d7b0c31
Bianca1232023 <santanabianca447@gmail.com>
2026-04-13 02:39:23 -0300

    restaurando quarta palavra perdida

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-const encoded = "ZXNwZXJvIHF1ZQ==";
+const encoded = "cGFydF9kb2NrZXI=";
 const decoded = Buffer.from(encoded, "base64").toString();
 
 app.get("/", (req, res) => {
